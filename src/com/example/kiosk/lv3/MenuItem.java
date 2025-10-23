7bbe4b29179674c3d8f603fbec5d52d2b881e039
sseung0510 <choiseunghi0510@gmail.com>
2025-10-23 12:03:13 +0900

    lv3 - Kiosk 클래스로 기능 분리

---
+++ src/com/example/kiosk/lv3/MenuItem.java
@@ -0,0 +1,28 @@
+package com.example.kiosk.lv3;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAll(){
+        return name + " | w " + price + " | " + description;
+    }
+
+}
