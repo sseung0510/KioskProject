d95343590d1b0ef4b577cf8ca3bcb441b6e7b918
sseung0510 <choiseunghi0510@gmail.com>
2025-10-28 15:11:37 +0900

    도전 - cart 클래스로 기능 분리 및 장바구니, 주문 기능 추가

--- src/com/example/kioskChallenge/lv1/Kiosk.java
+++ src/com/example/kioskChallenge/lv1/Kiosk.java
@@ -1,6 +1,5 @@
 package com.example.kioskChallenge.lv1;
 
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -13,9 +12,7 @@ public class Kiosk {
     // 속성
     private final List<Menu> menuList;
     private final Scanner sc = new Scanner(System.in);
-    private final List<String> cartMenu = new ArrayList<>(); // 장바구니 메뉴
-    private final List<Double> cartPrice = new ArrayList<>(); // 장바구니 가격 
-
+    private final Cart cart = new Cart();
 
     // 생성자
     public Kiosk(List<Menu> menuItemList) {
@@ -32,19 +29,19 @@ public class Kiosk {
         while(true) {
             showMenuList(); // 메뉴 조회 메서드
 
-            int menuNum = inputNumException(); // 숫자 제외 예외처리 메서드
+            int menuNum = inputNumException("메뉴판의 번호만 선택해주세요: "); // 숫자 제외 예외처리 메서드
 
             // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
             if(menuNum == 0) { // 종료
                 break; // 상위 반복문 탈출
-            } else if((menuNum == 4 || menuNum == 5) && cartMenu.isEmpty()) {
+            } else if((menuNum == 4 || menuNum == 5) && cart.isCartEmpty()) {
                 System.out.println("없는 메뉴입니다.");
                 continue;
             } else if(menuNum == 4) {
                 orderMenu();
                 continue;
             } else if(menuNum == 5) {
-                cartMenu.clear();
+                cart.clearCart();
                 continue;
             } else if (menuNum < 1 || menuNum > menuList.size()) {
                 System.out.println("없는 메뉴입니다.");
@@ -58,7 +55,7 @@ public class Kiosk {
 
             while(true) {
                 // 숫자 제외 예외처리 메서드
-                int menuItemNum = inputNumException();
+                int menuItemNum = inputNumException("메뉴판의 번호만 선택해주세요: ");
 
                 if(menuItemNum == 0) {
                     break;
@@ -67,14 +64,21 @@ public class Kiosk {
                     continue;
                 }
                 MenuItem menuItem = menu.getMenuList().get(menuItemNum - 1);
-                System.out.println("선택한 메뉴: " + menuItem.getAll() + "\n");
+                System.out.println("선택한 메뉴: " + menuItem.getAll());
 
-                System.out.println('"' + menuItem.getAll() + '"' + "\n위 메뉴를 장바구니에 추가하시겠습니까?");
+                int menuCount = inputNumException("수량 선택: ");
+
+                if(menuCount <= 0) {
+                    System.out.println("1개 이상부터 주문이 가능합니다.");
+                    continue;
+                }
+
+                System.out.println();
+                System.out.println('"' + menuItem.getAll() + '"' + "\t" + menuCount + "개 \n위 메뉴를 장바구니에 추가하시겠습니까?");
                 System.out.println("1. 확인    |    2. 취소");
                 int choiceNum = sc.nextInt();
                 if(choiceNum == 1) {
-                    cartMenu.add(menuItem.getAll());
-                    cartPrice.add(menuItem.getPrice());
+                    cart.addCart(menuItem.getAll(), menuItem.getPrice(), menuCount);
                     System.out.println(menuItem.getName() + "(이)가 장바구니에 추가되었습니다.");
                     continue FIRST;
                 } else if(choiceNum == 2) {
@@ -90,26 +94,25 @@ public class Kiosk {
      * 메뉴 주문 기능
      */
     private void orderMenu() {
-        double paySum = 0;
+        double totalPay = 0;
 
         System.out.println("아래와 같이 주문 하시겠습니까?\n");
         System.out.println("[ 주문 ]");
-        for(int i = 0; i < cartMenu.size(); i++) {
-            System.out.println(cartMenu.get(i));
-        }
+        cart.printCart();
 
-        for(int i = 0; i < cartMenu.size(); i++) {
-            paySum += cartPrice.get(i);
+        for(int i = 0; i < cart.getCartPrice().size(); i++) {
+            totalPay += cart.getCartPrice().get(i) * cart.getCartCount().get(i);
         }
 
         System.out.println("\n[ 총 ]");
-        System.out.println("w " + paySum);
+        System.out.println("w " + totalPay);
 
         System.out.println("\n1. 주문    |    2. 메뉴판");
-        int choiceNum = inputNumException();
+        int choiceNum = inputNumException("해당 번호만 선택해주세요: ");
+
         if(choiceNum == 1) {
-            System.out.println("주문이 완료되었습니다. 금액은 w" + paySum + "입니다.");
-            cartMenu.clear();
+            System.out.println("주문이 완료되었습니다. 금액은 w" + totalPay + "입니다.");
+            cart.clearCart();
         } else if(choiceNum == 2) {
             System.out.println();
         } else {
@@ -126,7 +129,7 @@ public class Kiosk {
             System.out.println((i + 1) + ". " + menuList.get(i).getCategory());
         }
         System.out.println("0. 종료    | 종료");
-        if(!cartMenu.isEmpty()) {
+        if(!cart.isCartEmpty()) {
             System.out.println();
             System.out.println("[ ORDER MENU ]");
             System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
@@ -137,11 +140,11 @@ public class Kiosk {
     /**
      * 숫자 제외 입력 예외처리 기능
      */
-    public int inputNumException() {
+    public int inputNumException(String message) {
         int num;
         while(true){ // 숫자만 입력받을때까지 반복
             try { // 숫자가 아닌 값이 들어오면 예외처리
-                System.out.print("원하시는 메뉴를 골라주세요: ");
+                System.out.print(message);
                 num = sc.nextInt();
                 break;
             } catch (InputMismatchException e){
